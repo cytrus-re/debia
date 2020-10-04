9a131aac7d31fcad1adfe17cb48545445c1dcbf6
QuantumlyTangled <ethoarx@gmail.com>
2020-10-04 15:01:31 +0200

    fix: jest error

--- README.md
+++ README.md
@@ -1,9 +1,2 @@
 # debia
 A multipurpose Discord bot
-
-# Note
-
-## Strings to replace
-A multipurpose Discord bot
-In BotClient.ts you can change Bot to the name of your project but do be careful about correcting the imports in other files.
-
--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,7 @@ module.exports = {
 	testRunner: 'jest-circus/runner',
 	testMatch: ['<rootDir>/tests/**/*.test.ts'],
 	moduleNameMapper: {
-		'^@utils/(.*)$': '<rootDir>/src/lib/util/$1',
+		'^@utils/(.*)$': '<rootDir>/src/lib/utils/$1',
 		'^@lib/(.*)$': '<rootDir>/src/lib/$1',
 		'^@root/(.*)$': '<rootDir>/src/$1'
 	},
