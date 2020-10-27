719ab45ba00cadc3176e110fda2e0dd4330304d9
Devnol <pgkoutsoumanis@gmail.com>
2020-10-27 12:39:50 +0200

    ci: let's try that

--- scripts/decrypt_secret.sh
+++ scripts/decrypt_secret.sh
@@ -5,4 +5,4 @@ mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$SA_KEY" \
---output $HOME/secrets/GCE_SA.json ./GCE_SA.json.gpg
+--output $HOME/secrets/GCE_SA.json ./scripts/GCE_SA.json.gpg
