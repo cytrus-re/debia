81e1d684cf7e35b74bb93c8c43cefd6d355b4eb2
Oxygemo <Oxygemo@users.noreply.github.com>
2021-01-05 06:46:45 +0000

    TSC build: 4ceb64af63975f361335e344dbd0c1ce7ad2aaa3

--- dist/index.js
+++ dist/index.js
@@ -10,6 +10,6 @@ async function main() {
 }
 console.log('Debia is in town!');
 console.log('Yeah, you heard right');
-console.log("what is this cringe....");
+console.log('what is this cringe...."');
 utils_1.floatPromise({ client }, main());
 //# sourceMappingURL=index.js.map
