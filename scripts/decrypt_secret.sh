b9c38f6503dbdc7f174d6ca701779735cd0b4ea4
Devnol <pgkoutsoumanis@gmail.com>
2021-01-05 09:19:06 +0200

    ci: get rid of old sketchy CI
    Signed-off-by: Devnol <pgkoutsoumanis@gmail.com>

--- scripts/decrypt_secret.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Decrypt the file
-mkdir $HOME/secrets
-# --batch to prevent interactive command
-# --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$SA_KEY" \
---output $HOME/secrets/GCE_SA.json ./scripts/GCE_SA.json.gpg
